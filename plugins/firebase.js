778136144b60b417c04632521aecfa430dca9611
toshihirokato <nanamin.104@icloud.com>
2020-02-27 19:40:44 +0900

    Firebase の初期化を是正

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 
-if(!firebase.app.length) {
+if(!firebase.apps.length) {
   const firebaseConfig = {
     apiKey: "AIzaSyDPtgGX2peTSXI2kv0T88iTuQlm8ps4IJY",
     authDomain: "slack-clone-app-95304.firebaseapp.com",
