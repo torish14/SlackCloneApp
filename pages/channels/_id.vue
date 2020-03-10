113daad1822cfc2aea899def1faadccaf37f8e06
toshihirokato <nanamin.104@icloud.com>
2020-03-11 03:23:32 +0900

    リアルタイムのチャット機能

--- pages/channels/_id.vue
+++ pages/channels/_id.vue
@@ -28,12 +28,18 @@ export default {
   mounted() {
     //! this.$route は現在の url に関する情報を取得できる。
     const channelId = this.$route.params.id
-    db.collection('channels').doc(channelId).collection('messages').get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          this.messages.push({ id: doc.id, ...doc.data() })
+    db.collection('channels').doc(channelId).collection('messages')
+      // onSnapshot を collection('messages') に対して実行する。
+      // つまり、messages collection に変更があった場合に処理を実行する。
+      .onSnapshot((snapshot) => {
+        // collection に変更があった場合に snapshot がコールバック関数に渡される。
+        // snapshot とは DB のコピーのようなもの。
+        snapshot.docChanges().forEach((change) => {
+          const doc = change.doc
+          if(change.type === 'added') {
+            this.messages.push({ id: doc.id, ...doc.data() })
+          }
         })
-        console.log(this.messages) // 開発用に
       })
   }
 }
