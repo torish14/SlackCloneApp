b8d650c52bab3a435688e69c72fa03392a9a4164
toshihirokato <nanamin.104@icloud.com>
2020-03-11 03:19:04 +0900

    collection の取得名を messages に変更

--- components/ChatForm.vue
+++ components/ChatForm.vue
@@ -18,7 +18,7 @@ export default {
     addMessage(event) {
       if (this.keyDownedForJPConversion(event)) { return }
       const channelId = this.$route.params.id
-      db.collection('channels').doc(channelId).collection('message').add({ text: this.text })
+      db.collection('channels').doc(channelId).collection('messages').add({ text: this.text })
         .then(() => {
           this.text = null
         })
